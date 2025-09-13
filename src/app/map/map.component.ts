25e6219fb94e40a2d6afb4f5b968b3d538ef1a57
Bart van Pelt <bart.vanpelt@gmail.com>
2025-09-13 18:41:42 +0200

    Startup

---
+++ src/app/map/map.component.ts
@@ -0,0 +1,43 @@
+import { Component,OnInit } from '@angular/core';
+
+import Map from 'ol/Map.js';
+import OSM from 'ol/source/OSM.js';
+import TileLayer from 'ol/layer/Tile.js';
+import View from 'ol/View.js';
+
+@Component({
+  selector: 'app-map',
+  imports: [],
+  templateUrl: './map.component.html',
+  styleUrl: './map.component.css'
+})
+export class MapComponent {
+
+  private map: Map = new Map();
+
+  constructor() {
+    
+  }
+
+   ngOnInit(): void {
+    console.log('MapComponent start initialization');
+    this.initializeMap();
+  }
+
+  private initializeMap(): void {
+    this.map = new Map({
+      target: 'map',
+      layers: [
+        new TileLayer({
+          source: new OSM(),
+        }),
+      ],
+      view: new View({
+        center: [0, 0],
+        zoom: 2,
+      }),
+    });
+  }
+
+
+}
